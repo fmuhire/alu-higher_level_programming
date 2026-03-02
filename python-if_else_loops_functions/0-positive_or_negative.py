215cfa1f1c1523792defbada8e933dfb022636ac
fmuhire <f.muhire@alustundent.com>
2026-03-02 08:45:24 -0800

    Add 0-positive_or_negative.py to check positive and negative numbers

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,9 @@ import random
 
 number = random.randint(-10, 10)
 
-if number > 0:
+if type(number) != int:
+    print("Wrong type")
+elif number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
