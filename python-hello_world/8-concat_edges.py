868f0511c404a1ee6aedb0741bbe38a014012c68
fmuhire <f.muhire@alustundent.com>
2026-03-02 06:32:05 -0800

    Add 8-concat.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str1 = "object-oriented "
-str2 = "programming "
-str3 = "with "
-str4 = "Python"
-print(str1 + str2 + str3 + str4)
+str2 = "programming with Python"
+print(str1 + str2)
